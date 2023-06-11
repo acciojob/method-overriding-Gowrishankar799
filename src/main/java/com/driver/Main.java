34720d04a1b8ee236af5486b135853f5d0487814
Gowrishankar799 <130752694+Gowrishankar799@users.noreply.github.com>
2023-06-11 12:45:01 +0530

    method overriding runtime polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
+        B b1 = new B();
+        System.out.println(b1.meth());
+        System.out.println(b1.meth());
+    }
+
   
 }
