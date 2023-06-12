b5107aabe86a35079cdf6f96ec2140b45b9ee298
Gowrishankar799 <130752694+Gowrishankar799@users.noreply.github.com>
2023-06-12 06:25:07 +0530

    commit changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String args[]){
         B b1 = new B();
-        System.out.println(b1.meth());
+        System.out.println(A.meth());
         System.out.println(b1.meth());
     }
 
